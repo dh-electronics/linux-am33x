9826e582fe9f2e43bbb059a1281ce44952164d1a
Ludwig Zenz <lzenz@dh-electronics.de>
2014-11-07 14:38:10 +0100

    mmc: set /dev/mmcblkX via device-tree aliases
    Conflicts:
    	arch/arm/boot/dts/imx6qdl.dtsi
    
    Conflicts:
    	drivers/mmc/host/omap_hsmmc.c

--- drivers/mmc/host/sdhci-esdhc-imx.c
+++ drivers/mmc/host/sdhci-esdhc-imx.c
@@ -868,9 +868,19 @@ sdhci_esdhc_imx_probe_dt(struct platform_device *pdev,
 			 struct esdhc_platform_data *boarddata)
 {
 	struct device_node *np = pdev->dev.of_node;
+	int ret;
 
 	if (!np)
 		return -ENODEV;
+
+	ret = of_alias_get_id(np, "mmcblk");
+	if (ret < 0) {
+		dev_err(&pdev->dev, "failed to get alias id, errno %d\n", ret);
+		return -ENODEV;
+	}
+
+	boarddata->devidx = ret;
+
 
 	if (of_get_property(np, "non-removable", NULL))
 		boarddata->cd_type = ESDHC_CD_PERMANENT;
@@ -1011,7 +1021,10 @@ static int sdhci_esdhc_imx_probe(struct platform_device *pdev)
 		}
 		imx_data->boarddata = *((struct esdhc_platform_data *)
 					host->mmc->parent->platform_data);
-	}
+	} 
+	
+	/* copy mmc block device index */
+	host->mmc->devidx = boarddata->devidx;
 
 	/* card_detect */
 	if (boarddata->cd_type == ESDHC_CD_CONTROLLER)
